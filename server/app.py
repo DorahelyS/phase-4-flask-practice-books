bf29607a57aa7a3ae4f6090c0d4204cc38d64c69
Dorahely Sanchez <dorahelys@gmail.com>
2024-01-17 17:37:57 -0600

    completed

--- server/app.py
+++ server/app.py
@@ -22,7 +22,7 @@ api = Api(app)
 def index():
     return "Hello world"
 
-'''
+
 # write your routes here!
 #GET /authors/:id
 #Returns an author with the matching id. If there is no author, returns a message that the 
@@ -50,12 +50,13 @@ def author_by_id(id):
 
     return response
 
+
 #DELETE /author/:id
 #Deletes the author and all associated books from the database. Returns 204 if the author 
 #was successfully deleted or 404 and an appropriate message if that author could not be found.
 #COULD ADD IT IN THE GET METHOD AND COMBINE BOTH GET & DELETE ??
 @app.route('/authors/<int:id>', methods = ['DELETE'])
-def author_by_id(id):
+def delete_author_by_id(id):
     author = Author.query.filter(Author.id == id).first()
     #have to include a conditional if no author is found
     #first if auther even exist create response that includes the auther in JSON
@@ -87,16 +88,17 @@ def author_by_id(id):
     return response
 
 
+'''
 #IF I DID NOT HAVE A POST & IT WAS JUST A GET
 #GET /books
-@app.route('/books', method = ['GET'])
+@app.route('/books', methods = ['GET'])
 def books():
     #need to hit the db to get list of all the campers
     books = Book.query.all()
     #need to make it a dict so that it can become json 
     #since it it currently a list going to use list comprehension to call on each object
     #since to_dict can only be called on one object not a list
-    books_dict = [book.to_dict(rules = ('-author', '-publisher')) for book in books]
+    books_dict = [book.to_dict() for book in books]
 
     #can write like this too:
     #books_dict = []
@@ -104,11 +106,12 @@ def books():
         #books_dict.append(book.to_dict(rules = ('-author', '-publisher')))
     #creating response
     response = make_response(
-        books_dic,
+        books_dict,
         200
     )
     return response
 
+'''
 
 #GET /books
 @app.route('/books', methods = ['GET', 'POST'])
@@ -136,14 +139,14 @@ def books():
         #creat error handlingwith try/except
         try:
             #now to post - line line gets the info from the front end
-            form_data = request.getjson()
+            form_data = request.get_json()
             #now creating new book object
             new_book = Book(
                 title = form_data['title'],
                 page_count = form_data['page_count'],
                 author_id = form_data['author_id'],
-                publisher_id = form_date['author_id']
-            )
+                publisher_id = form_data['publisher_id']
+                )
             db.session.add(new_book)
             db.session.commit()
 
@@ -152,7 +155,7 @@ def books():
                 201
             )
 
-        except ValueErorr:
+        except ValueError:
             response = make_response(
                 {"error": "not found"},
                 400
@@ -160,11 +163,26 @@ def books():
 
     return response
 
+'''
 #POST /books
 #Creates a new book. The book must belong to an author and a publisher. Return the new book details like so:
 #ADDING TO GET BOOKS METHOD 
+@app.post('/books')
+def post_book():
+    try: 
+        data = request.get_json() 
+        new_book = Book(title=data['title'], 
+                        page_count=data['page_count'], 
+                        author_id=data['author_id'], 
+                        publisher_id=data['publisher_id']
+                    )
+        db.session.add(new_book)
+        db.session.commit()
+        return make_response(new_book.to_dict(), 201)
+    except Exception:
+        return make_response({'error': Exception.with_traceback}, 406)
 
-
+'''
 #GET /publishers/:id
 #Returns a publisher with the matching id. If there is no publisher, returns a message that 
 #the publisher could not be found along with a 404.
@@ -190,7 +208,5 @@ def publisher_by_id(id):
 
     return response
 
-
-'''
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
